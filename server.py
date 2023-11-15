498338c86ba4be2cacf6e4ee15f2cbf17e98e842
satish <satishnaikar12@gmail.com>
2023-11-15 14:00:10 +0530

    my portfo

---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask,render_template,request,redirect
+import csv
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/<string:page_name>')
+def about(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email=data["email"]
+        subject=data["subject"]
+        message= data["message"]
+        file=database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a',newline='') as database2:
+        email=data["email"]
+        subject=data["subject"]
+        message= data["message"]
+        csv_writer= csv.writer(database2,delimiter=',',quotechar='|',quoting=csv.QUOTE_NONE)
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+    if request.method=='POST':
+        try:
+            data =request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not saved into Database'
+    else:
+        return "Something went wrong"
+
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
